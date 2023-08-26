f28b8afe1d81d9ad5093be3ac153929eb4d640a5
Madan <madan.patakota@gmail.com>
2023-08-26 11:43:36 +0530

    Angular Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Batch8-AngularForms';
+
+  
+  evtSubmit(form:NgForm){
+    console.log("your form is " , form);
+  }
+
+
+  //Here angular provide some status to the element
+  //those status list
+  
+  //valid 
+  //Invalid
+  //pristine
+  //dirty
+  //touched
+  //untouched
+
+
+
 }
